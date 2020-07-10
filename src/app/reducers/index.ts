97a96a26b071d9749684b1108ab3b65fdfeae467
yurakhomitsky <yura.khomitsky8@gmail.com>
2020-07-10 17:46:16 +0300

    removed useless

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -4,58 +4,18 @@ import {
   createFeatureSelector,
   createSelector,
   MetaReducer,
-  Action
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
-import { localStorageSync } from 'ngrx-store-localstorage';
-import { newTable } from '../store/actions/store.actions';
-import { storage } from '../utils/utils';
-import { routerReducer } from '@ngrx/router-store';
+  Action,
+} from "@ngrx/store";
+import { environment } from "../../environments/environment";
+import { localStorageSync } from "ngrx-store-localstorage";
+import { newTable } from "../store/actions/store.actions";
+import { storage } from "../utils/utils";
+import { routerReducer } from "@ngrx/router-store";
 
-
-export interface AppState { }
+export interface AppState {}
 
 export const reducers: ActionReducerMap<AppState> = {
-  router: routerReducer
-};
-export function log(reducer: ActionReducer<AppState>): ActionReducer<AppState> {
-  return (state: AppState, action: Action): AppState => {
-    console.log(state);
-    return reducer(state, action);
-  };
-}
-
-export function syncStorage(reducer: ActionReducer<AppState>): ActionReducer<AppState> {
-  return (state: AppState, action: any): AppState => {
-    
-    return reducer(state, action);
-  };
-}
-export const mergeReducer = (state: AppState, rehydratedState: any, action: any) => {
-  let stateFromStorage;
-  let link;
-  if (action.type === '[DASHBOARD] Table link') {
-    link = action.link;
-    stateFromStorage = storage(link);
-    if (!stateFromStorage) {
-      storage(link, state);
-      rehydratedState = storage(link);
-    }
-    state = stateFromStorage;
-  }
+  router: routerReducer,
 };
 
-export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
-
-  return localStorageSync({
-    keys: ['excel'],
-    rehydrate: true,
-    syncCondition: (state) => {
-      // console.log('Sync', state);
-      return true;
-    },
-    mergeReducer
-  })(reducer);
-
-}
 export const metaReducers: MetaReducer<AppState>[] = [];
